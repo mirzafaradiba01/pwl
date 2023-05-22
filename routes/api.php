be429f9f003847e13df4741cc04df072264a0833
Mirza Priscilla Faradiba <mirzapriscilla5@gmail.com>
2023-05-22 12:00:33 +0700

    Pertemuan 11 - Praktikum 2

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/todos', TodoController::class);
 });
 
 Route::post('/register', [AuthController::class, 'register']);
